39ac2e7fe03aea2e1221501fd205ee9e11f9c2f2
fernanduandrade <onanduandrade@gmail.com>
2024-10-28 20:51:14 -0300

    feat: add draggable element

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const cursor = document.getElementById('js-cursor')
 const cursorSmall = document.querySelector('.cursor__small')
 const dot = document.querySelector('.dot')
-const divTest = document.getElementById('teste')
 
 let mouseX = 0, mouseY = 0
 let cursorX = 0, cursorY = 0
@@ -28,8 +27,47 @@ document.addEventListener('mousemove', (e) => {
     mouseY = e.pageY;
 });
 
+function onMouseDrag(e) {
+    let getContainerStyle = window.getComputedStyle(container);
+    let leftValue = parseInt(getContainerStyle.left);
+    let topValue = parseInt(getContainerStyle.top);
+    container.style.left = `${leftValue + movementX}px`;
+    container.style.top = `${topValue + movementY}px`;
+}
+
 [...document.querySelectorAll('.flow')].forEach(function (item) {
+    function onMouseDrag({ movementX, movementY }) {
+        let getStyle = window.getComputedStyle(item);
+        let leftValue = parseInt(getStyle.left);
+        let topValue = parseInt(getStyle.top);
+        item.style.left = `${leftValue + movementX}px`;
+        item.style.top = `${topValue + movementY}px`;
+    }
+
+    function leaveElement(e) {
+        dot.classList.remove('dot-merging');
+        animationId = requestAnimationFrame(animateCursor);
+        cursor.style.display = `block`
+        cursorSmall.style.display = 'block'
+        dot.classList.remove('dot_2')
+        dot.classList.add('dot')
+        dot.style.width = '10px'
+        dot.style.height = '10px'
+        dot.style.borderRadius = '50%'
+    }
+
+    item.addEventListener("mousedown", () => {
+        item.addEventListener("mousemove", onMouseDrag)
+        item.removeEventListener("moueleave", leaveElement)
+    });
+
+    document.addEventListener("mouseup", () => {
+        item.removeEventListener("mousemove", onMouseDrag)
+        item.addEventListener("moueleave", leaveElement)
+    });
+
     item.addEventListener('mouseenter', function (e) {
+        console.log(e)
         cancelAnimationFrame(animationId)
         target = e.currentTarget;
         dot.classList.add('dot-merging');
@@ -44,23 +82,11 @@ document.addEventListener('mousemove', (e) => {
         cursor.style.display = `none`
         cursorSmall.style.display = 'none'
 
-        target.style.cursor = 'pointer'
+        target.style.cursor = 'grab'
         target.style.border = '6px white solid'
         target.style.transition = 'ease .1s';
     });
+
 });
 
-[...document.querySelectorAll('.flow')].forEach(function (item) {
-    item.addEventListener('mouseleave', function (e) {
-        dot.classList.remove('dot-merging');
-        animationId = requestAnimationFrame(animateCursor);
-        cursor.style.display = `block`
-        cursorSmall.style.display = 'block'
-        dot.classList.remove('dot_2')
-        dot.classList.add('dot')
-        dot.style.width = '10px'
-        dot.style.height = '10px'
-        dot.style.borderRadius = '50%'
-    });
-});
 animateCursor()
